30b4726cf6c4ae49a5567935ee40399da1b6e009
= <hardlane2000@gmail.com>
2023-06-29 19:01:24 +0200

    improving code structure and formatting

--- static/js/hangman.js
+++ static/js/hangman.js
@@ -1,132 +1,114 @@
-var letterElements = document.getElementsByClassName("letter_element");
-var letterButtons = document.getElementsByClassName("letter_button");
-var randomQuiz = "";
-randomGenre = "";
-randomQuiz = "";
-var attempts = document.getElementsByClassName("attempt");
-var unsuccessfulAttempts = 0;
-var hangmanGrid = document.getElementById("hangman");
-var glitchImg = document.getElementById("glitch_img");
-var glitchLayers = document.getElementById("glitch_layers");
-var title = document.getElementsByClassName("title")[0];
-var glitchText = document.getElementsByClassName("glitch_text")[0];
-var stage = document.getElementById("stage");
-var currentStage = 1;
-var stageCompletedText = document.getElementById("glitch_text_1");
-var allCompletedElement = document.getElementById("glitch_text_2");
-var allCompletedText = document.getElementById("glitch_text_content");
-var randomIndex = 0;
+const letterElements = document.getElementsByClassName('letter_element');
+const letterButtons = document.getElementsByClassName('letter_button');
+const attempts = document.getElementsByClassName('attempt');
+const hangmanGrid = document.getElementById('hangman');
+const glitchImg = document.getElementById('glitch_img');
+const glitchLayers = document.getElementById('glitch_layers');
+const title = document.getElementsByClassName('title')[0];
+const glitchText = document.getElementsByClassName('glitch_text')[0];
+const stage = document.getElementById('stage');
+const stageCompletedText = document.getElementById('glitch_text_1');
+const allCompletedElement = document.getElementById('glitch_text_2');
+const allCompletedText = document.getElementById('glitch_text_content');
+const quizQuestion = document.getElementById("quiz_question");
+
+let randomQuiz = '';
+let randomGenre = '';
+let randomIndex = 0;
+let unsuccessfulAttempts = 0;
+let currentStage = 1;
 
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
-for (let i = 0; i < letterButtons.length; i++) letterButtons[i].innerText = alphabet[i];
+for (let i = 0; i < letterButtons.length; i++) {
+  letterButtons[i].innerText = alphabet[i];
+}
 
 function newQuestion() {
-
-    randomQuiz = quiz[Math.floor(Math.random() * quiz.length)];
-//    console.log(randomQuiz);
-//    console.log(quiz);
-    randomIndex = quiz.indexOf(randomQuiz);
-    randomGenre = randomQuiz["genre"];
-    randomQuiz = randomQuiz["question"];
-    quizTitle = document.getElementById("quiz_title");
-    quizTitle.innerText = randomGenre;
-    quizQuestion = document.getElementById("quiz_question");
-    var questionContent = "";
-    for (const letterButton of letterButtons) letterButton.classList.remove("tested");
-    unsuccessfulAttempts = 0;
-    for (let i = 0; i < randomQuiz.length; i++) {
-
-        (RegExp(/^\p{L}/,'u').test(randomQuiz[i]))
-        ? questionContent+= "_"
-        : questionContent+= randomQuiz[i]
-
-    }
-
-    //console.log(questionContent);
-
-    quizQuestion.innerText = questionContent;
-    //console.log(quizQuestion.innerText.length);
-    stage.innerText = `Current stage: ${currentStage}/10`;
-
-
+  randomQuiz = quiz[Math.floor(Math.random() * quiz.length)];
+  randomIndex = quiz.indexOf(randomQuiz);
+  randomGenre = randomQuiz['genre'];
+  randomQuiz = randomQuiz['original_title'];
+  const quizTitle = document.getElementById('quiz_title');
+  quizTitle.innerText = randomGenre;
+  const quizQuestion = document.getElementById('quiz_question');
+  let questionContent = '';
+  for (const letterButton of letterButtons) {
+    letterButton.classList.remove('tested');
+  }
+  unsuccessfulAttempts = 0;
+  for (let i = 0; i < randomQuiz.length; i++) {
+    questionContent += RegExp(/^\p{L}/, 'u').test(randomQuiz[i]) ? '_' : randomQuiz[i];
+  }
+  quizQuestion.innerText = questionContent;
+  stage.innerText = `Current stage: ${currentStage}/10`;
 }
 
 newQuestion();
 
 function checkLetter(event) {
-
-    var letter = event.target.innerText;
-    event.target.classList.add("tested");
-    (randomQuiz.toUpperCase().includes(letter)) ? successfulAttempt(letter) : unsuccessfulAttempt(letter);
-    if (quizQuestion.innerText == randomQuiz.toUpperCase()) { (currentStage == 10) ? allIsNowComplete() : questionCompleted() }
-
+  const letter = event.target.innerText;
+  event.target.classList.add('tested');
+  randomQuiz.toUpperCase().includes(letter) ? successfulAttempt(letter) : unsuccessfulAttempt(letter);
+  if (quizQuestion.innerText === randomQuiz.toUpperCase()) {
+    currentStage === 10 ? allIsNowComplete() : questionCompleted();
+  }
 }
 
 function unsuccessfulAttempt(letter) {
-
-    unsuccessfulAttempts++;
-    attempts[unsuccessfulAttempts - 1].style.color = "red";
-    if (unsuccessfulAttempts == 4) questionFailed();
-
+  unsuccessfulAttempts++;
+  attempts[unsuccessfulAttempts - 1].style.color = 'red';
+  if (unsuccessfulAttempts === 4) questionFailed();
 }
 
 function successfulAttempt(letter) {
-
-    for (let i = 0; i < randomQuiz.length; i++) if (letter == randomQuiz[i].toUpperCase()) { quizQuestion.innerText = quizQuestion.innerText.replaceAt(i, letter); }
-
+  for (let i = 0; i < randomQuiz.length; i++) {
+    if (letter === randomQuiz[i].toUpperCase()) {
+      quizQuestion.innerText = quizQuestion.innerText.replaceAt(i, letter);
+    }
+  }
 }
 
 async function questionCompleted() {
-
-    hangmanGrid.style.display = "none";
-    currentStage++;
-    stageCompletedText.style.display = "block";
-    quiz.splice(randomIndex, 1);
-    console.log(quiz);
-    await getGame(1200);
-    hangmanGrid.style.display = "block";
-    stageCompletedText.style.display = "none";
-    for (const attempt of attempts) attempt.style.color = "inherit";
-    newQuestion();
-
+  hangmanGrid.style.display = 'none';
+  currentStage++;
+  stageCompletedText.style.display = 'block';
+  quiz.splice(randomIndex, 1);
+  await wait(1200);
+  hangmanGrid.style.display = 'block';
+  stageCompletedText.style.display = 'none';
+  for (const attempt of attempts) {
+    attempt.style.color = 'inherit';
+  }
+  newQuestion();
 }
 
 async function allIsNowComplete() {
-
-    hangmanGrid.style.display = "none";
-    currentStage++;
-    allCompletedElement.style.display = "block";
-    allCompletedText.innerText = "Congratulations!";
-    await getGame(1360);
-    allCompletedText.innerText = "Most people are so ungrateful to be alive...";
-    await getGame(1360);
-    allCompletedText.innerText = "But not you...";
-    await getGame(1360);
-    allCompletedText.innerText = "Not anymore...";
-    await getGame(1200);
-
+  hangmanGrid.style.display = 'none';
+  currentStage++;
+  allCompletedElement.style.display = 'block';
+  allCompletedText.innerText = 'Congratulations!';
+  await wait(1360);
+  allCompletedText.innerText = 'Most people are so ungrateful to be alive...';
+  await wait(1360);
+  allCompletedText.innerText = 'But not you...';
+  await wait(1360);
+  allCompletedText.innerText = 'Not anymore...';
+  await wait(1200);
 }
 
 async function questionFailed() {
-
-    hangmanGrid.style.display = "none";
-    glitchImg.style.display = "block";
-    glitchLayers.style.display = "block";
-    title.style.display = "none";
-    glitchText.style.display = "block";
-
+  hangmanGrid.style.display = 'none';
+  glitchImg.style.display = 'block';
+  glitchLayers.style.display = 'block';
+  title.style.display = 'none';
+  glitchText.style.display = 'block';
 }
 
-String.prototype.replaceAt = function(index, replacement) {
-
-    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
 
-function getGame(ms) {
-
-    return new Promise(resolve => setTimeout(resolve, ms));
-
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-
